07603e93a85c021c67c906a7a81e574d94db6c8f
nlloydw <nlloydw61@gmail.co.uk>
2022-07-13 22:35:43 +0100

    completed

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,19 @@ module.exports = {
   ],
   theme: {
     extend: {
+        colors:
+        {
+            'cyan':'hsl(179, 62%, 43%)',
+            'light-cyan':'#3abebc',
+            'bright-yellow':'hsl(71, 73%, 54%)',
+            'light-gray':'hsl(204, 43%, 93%)',
+            'grayish-blue':'hsl(218, 22%, 67%)'
+        }
+        ,
+        fontFamily:
+        {
+            'karla':['karla']
+        },
     },
   },
   plugins: [],
